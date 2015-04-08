0bb5e9551752ed9889ceebc57016414e2e392569
Brian Moriarty <bmoriarty@forwardslope.com>
2015-04-08 13:17:15 -0700

    Base functionality with a few sugar features... except for messy UICollectionView. Committing in this state so I can easily roll back if needed after experimenting with layout mechanics of UICollectionView. Code style and commenting need review as well. Feature List: - Navigation flow: implemented correctly - Meme editor: implemented correctly, with UIScrollView sugar. - Tableview: implemented correctly, base version... planning to add slide-in delete button later. - Meme Viewer: implemented correctly, with tap-gesture-fullscreen sugar... planned "Delete", planned "Edit". - Collection View: semi-functional, but poor layout needs fixing.

---
+++ MemeMe/MemeItemsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MemeItemsViewController.swift
+//  MemeMe
+//
+//  Created by Brian Moriarty on 4/5/15.
+//  Copyright (c) 2015 Brian Moriarty. All rights reserved.
+//
+
+import UIKit
+
+class MemeItemsViewController: UIViewController {
+    
+    var memes: [Meme]!
+    
+    // MARK: View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let object = UIApplication.sharedApplication().delegate
+        let appDelegate = object as AppDelegate
+        memes = appDelegate.memes
+        reloadMemes()
+    }
+    
+    // MARK: Helpers
+    
+    func showStaticViewerForMeme(meme: Meme) {
+        var singleMemeViewer = storyboard?.instantiateViewControllerWithIdentifier("MemeStaticViewer") as SingleMemeViewController
+        singleMemeViewer.meme = meme
+        navigationController?.pushViewController(singleMemeViewer, animated: true)
+    }
+    
+    //
+    
+    func reloadMemes() {
+        fatalError("This method must be overridden")
+    }
+    
+}
